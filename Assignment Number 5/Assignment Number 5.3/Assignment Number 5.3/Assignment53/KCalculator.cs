2a46d69e26dbdc9281048aa4da45e14b0dc80161
rjlgithub <bigcats96@hotmail.com>
2014-05-28 10:05:43 -0600

    Assignment Number 5.3

---
+++ Assignment Number 5/Assignment Number 5.3/Assignment Number 5.3/Assignment53/KCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment53
+{
+    public static class KCalculator
+    {
+        static public int Add(int value1, int value2)
+        {
+            return 1000 * (value1 + value2);
+        }
+        
+        static public int Subtract(int value1, int value2)
+        {
+            return 1000 * (value1 - value2);
+        }
+
+        static public int times1000(int value1)
+        {
+            return 1000 * value1;
+        }
+ 
+    }
+}
---
+++ Assignment Number 5/Assignment Number 5.3/Assignment Number 5.3/Assignment53/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 230;
+            int a2 = 5;
+            int s1 = 55;
+            int s2 = 7;
+
+            int aCalc = KCalculator.Add(a1, a2);
+            int sCalc = KCalculator.Subtract(s1, s2);
+
+            Console.WriteLine();
+            Console.WriteLine("{0} + {1} = {2} ", KCalculator.times1000(a1), KCalculator.times1000(a2), aCalc);
+            Console.WriteLine(" {0} - {1} =  {2} ", KCalculator.times1000(s1), KCalculator.times1000(s2), sCalc);
+        }
+    }
+}
