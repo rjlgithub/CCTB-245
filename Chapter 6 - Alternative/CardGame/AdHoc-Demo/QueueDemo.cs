9b74ee32673bee020ca1cfebf079e5c3151074b2
rjlgithub <bigcats96@hotmail.com>
2014-05-28 12:59:42 -0600

    Chapter 6 - Alternative modifications

--- Chapter 6 - Alternative/CardGame/AdHoc-Demo/QueueDemo.cs
+++ Chapter 6 - Alternative/CardGame/AdHoc-Demo/QueueDemo.cs
@@ -8,5 +8,32 @@ namespace CardGame.AdHoc_Demo
 {
     class QueueDemo
     {
+        public void Run()
+        {
+            Queue<string> items = new Queue<string>();
+            FillWithData(items);
+            EmptyAndDisplay(items);
+        }
+
+        // private methods
+        private void FillWithData(Queue<string> items)
+        {
+            items.Enqueue("First"); // Use the .Enqueue() method to add to the end
+            items.Enqueue("Second");
+            items.Enqueue("Third");
+            items.Enqueue("Fourth");
+        }
+
+        private void EmptyAndDisplay(Queue<string> data)
+        {
+            Console.WriteLine("There are " + data.Count + " items in the queue:");
+            foreach (string text in data)
+                Console.Write(text + ", ");
+            Console.WriteLine();
+            Console.WriteLine("... now emptying data...");
+            while (data.Count > 0)
+                Console.WriteLine(data.Dequeue());
+            Console.WriteLine();
+        }
     }
 }
